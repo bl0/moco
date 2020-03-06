79c7249add9de39c8566a641ae620cba7f999639
刘斌 <liubinthss@gmail.com>
2020-03-07 07:56:02 +0800

    Update train_eval_imagenet100_baseLR0.4_alpha0.99_crop0.08_k1281166_t0.1_AMPO1.sh

--- scripts/train_eval_imagenet100_baseLR0.4_alpha0.99_crop0.08_k1281166_t0.1_AMPO1.sh
+++ scripts/train_eval_imagenet100_baseLR0.4_alpha0.99_crop0.08_k1281166_t0.1_AMPO1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 data_dir="./data/imagenet100"
-output_dir="./output/imagenet100/baseLR0.4_alpha0.99_crop0.08_k1281166_t0.1_AMPO1"
+output_dir="./output/imagenet100/baseLR0.4_alpha0.99_crop0.08_kall_t0.1_AMPO1"
 python -m torch.distributed.launch --master_port 12347 --nproc_per_node=8 \
     train.py \
     --data-dir ${data_dir} \
@@ -9,7 +9,7 @@ python -m torch.distributed.launch --master_port 12347 --nproc_per_node=8 \
     --base-learning-rate 0.4 \
     --alpha 0.99 \
     --crop 0.08 \
-    --nce-k 1281166 \
+    --nce-k 126689 \
     --nce-t 0.1 \
     --amp-opt-level O1 \
     --output-dir ${output_dir}
